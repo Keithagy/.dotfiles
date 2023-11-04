76b2d94cb83a737b21068183f012409ff8a6e5e3
Keith Ang <keithang@keiths-mbp.lan>
2023-11-04 12:29:01 +0800

    use lazyvim extras for rust + eslint

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -31,19 +31,20 @@ return {
       },
     },
     setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
+      -- commented out so as to not clash with lazyvim's eslint formatting extra
+      -- eslint = function()
+      --   require("lazyvim.util").lsp.on_attach(function(client, bufnr)
+      --     vim.api.nvim_create_autocmd("BufWritePre", {
+      --       buffer = bufnr,
+      --       command = "EslintFixAll",
+      --     })
+      --     if client.name == "eslint" then
+      --       client.server_capabilities.documentFormattingProvider = true
+      --     elseif client.name == "tsserver" then
+      --       client.server_capabilities.documentFormattingProvider = false
+      --     end
+      --   end)
+      -- end,
     },
   },
 }
